76d3ae1f6401553bde7448cabf09353755b86b00
Abdulkareem Mustapha <aphatheology@gmail.com>
2020-08-23 18:58:39 +0100

    Updates

---
+++ precache-manifest.81165c21b249eda47160cc7cb6b6772c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0244621790694dc9c135f635785edcb6",
+    "url": "/minimalistportfolio/index.html"
+  },
+  {
+    "revision": "5dc2a8c76e5781f6b95b",
+    "url": "/minimalistportfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f90ee321d07053b7fd7d",
+    "url": "/minimalistportfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5dc2a8c76e5781f6b95b",
+    "url": "/minimalistportfolio/static/js/2.f1836ba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minimalistportfolio/static/js/2.f1836ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90ee321d07053b7fd7d",
+    "url": "/minimalistportfolio/static/js/main.9ab94b43.chunk.js"
+  },
+  {
+    "revision": "2a80e292875be6b0d1b2",
+    "url": "/minimalistportfolio/static/js/runtime-main.154fc00e.js"
+  }
+]);
